e286a314526673cfb0a7cd1bda79d3c5e61aec78
BurtonQin <bobbqqin@gmail.com>
2020-07-05 18:57:08 +0800

    add cargo subcommand

--- src/bin/cargo-lock-bug-detect.rs
+++ src/bin/cargo-lock-bug-detect.rs
@@ -1,7 +1,6 @@
 use std::ffi::OsString;
 use std::env;
 use std::process::Command;
-use rustc_version::VersionMeta;
 
 const CARGO_LOCK_BUG_DETECTOR_HELP: &str = r#"Detect double-lock&conflict-lock on MIR
 Usage:
@@ -9,7 +8,6 @@ Usage:
 Subcommands:
     double-lock              Detect double-lock bugs
     conflict-lock            Detect conflict-lock bugs
-    setup                    Only perform automatic setup, but without asking questions (for getting a proper libstd)
 Common options:
     -h, --help               Print this message
     -V, --version            Print version info and exit
@@ -32,12 +30,6 @@ fn show_help() {
 
 fn show_version() {
     println!("lock-bug-detect {}", "0.1.0");
-    //println!(
-    //    "lock-bug-detect {} ({} {})",
-    //    env!("CARGO_PKG_VERSION"),
-    //    env!("VERGEN_SHA_SHORT"),
-    //    env!("VERGEN_COMMIT_DATE")
-    //);
 }
 
 fn show_error(msg: String) -> ! {
@@ -53,31 +45,6 @@ fn cargo() -> Command {
 fn has_arg_flag(name: &str) -> bool {
     let mut args = std::env::args().take_while(|val| val != "--");
     args.any(|val| val == name)
-}
-
-/// Gets the value of a `--flag`.
-fn get_arg_flag_value(name: &str) -> Option<String> {
-    // Stop searching at `--`.
-    let mut args = std::env::args().take_while(|val| val != "--");
-    loop {
-        let arg = match args.next() {
-            Some(arg) => arg,
-            None => return None,
-        };
-        if !arg.starts_with(name) {
-            continue;
-        }
-        // Strip leading `name`.
-        let suffix = &arg[name.len()..];
-        if suffix.is_empty() {
-            // This argument is exactly `name`; the next one is the value.
-            return args.next();
-        } else if suffix.starts_with('=') {
-            // This argument is `name=value`; get the value.
-            // Strip leading `=`.
-            return Some(suffix[1..].to_owned());
-        }
-    }
 }
 
 fn in_cargo_lock_bug_detect() {
@@ -98,7 +65,7 @@ fn in_cargo_lock_bug_detect() {
         LockBugDetectCommand::DoubleLock => cmd.env("RUST_LOCK_DETECTOR_TYPE", "DoubleLockDetector"),
         LockBugDetectCommand::ConflictLock => cmd.env("RUST_LOCK_DETECTOR_TYPE", "ConflictLockDetector"),
     };
-    cmd.env("RUSTC", "/home/boqin/Projects/HackRust/rust-lock-bug-detector/target/release/rust-lock-bug-detector");
+    cmd.env("RUSTC", "rust-lock-bug-detector");
     cmd.env("RUST_BACKTRACE", "full");
     let mut args = std::env::args().skip(skip);
     while let Some(arg) = args.next() {
@@ -115,10 +82,6 @@ fn in_cargo_lock_bug_detect() {
     if !exit_status.success() {
         std::process::exit(exit_status.code().unwrap_or(-1))
     };
-}
-
-fn setup() {
-    todo!{};
 }
 
 fn main() {
