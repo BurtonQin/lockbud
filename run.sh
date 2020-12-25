3cfd7335d2607dea32f40cb4c72564906170993b
François Garillot <francois@garillot.net>
2020-12-25 09:23:49 -0800

    Replace relative reference to cargo_dir to an absolute one
    double quote array expansions

--- run.sh
+++ run.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
+# this script's location
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+
 if [ -z "$1" ]; then
-	echo "No detecting directory is provided"
-	exit 1
+        echo "No detecting directory is provided"
+        exit 1
 fi
 cargo build --release
 export RUSTC=${PWD}/target/release/rust-lock-bug-detector
@@ -12,26 +16,26 @@ export RUST_LOCK_DETECTOR_TYPE=DoubleLockDetector
 export RUST_LOCK_DETECTOR_BLACK_LISTS="cc"
 #export RUST_LOCK_DETECTOR_WHITE_LISTS="inter,intra,static_ref"
 
-cargo_dir_file=$(realpath cargo_dir.txt)
+cargo_dir_file=$(realpath $DIR/cargo_dir.txt)
 rm $cargo_dir_file
 touch $cargo_dir_file
 
 pushd "$1" > /dev/null
 #cargo clean
 cargo_tomls=$(find . -name "Cargo.toml")
-for cargo_toml in ${cargo_tomls[@]}
+for cargo_toml in "${cargo_tomls[@]}"
 do
 #	echo $cargo_toml
-	echo $(dirname $cargo_toml) >> $cargo_dir_file
+        echo $(dirname $cargo_toml) >> $cargo_dir_file
 done
 
 IFS=$'\n' read -d '' -r -a lines < ${cargo_dir_file}
-for cargo_dir in ${lines[@]}
+for cargo_dir in "${lines[@]}"
 do
-	echo ${cargo_dir}
-	pushd ${cargo_dir} > /dev/null
-	cargo check
-	popd > /dev/null
+        echo ${cargo_dir}
+        pushd ${cargo_dir} > /dev/null
+        cargo check
+        popd > /dev/null
 done
 popd > /dev/null
 
